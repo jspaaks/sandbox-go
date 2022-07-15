661a83e96c628577cee940082e726e0b9e414f49
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2022-07-15 14:22:55 -0400

    simple event booking app tutorial complete

--- main.go
+++ main.go
@@ -3,53 +3,135 @@ package main
 import (
 	"fmt"
 	"strings"
+	"sync"
+	"time"
 )
 
+type Booking struct {
+	firstName    string
+	lastName     string
+	emailAddress string
+	nTickets     uint
+}
+
+const conferenceName string = "Go Conference"
+
+var bookings = make([]Booking, 0)
+
 func main() {
-	const conferenceName string = "Go Conference"
+
 	const nTickets uint = 50
 	var nTicketsRemaining uint = nTickets
+	var waitGroup = sync.WaitGroup{}
 
-	fmt.Printf("Welcome to %s booking application\n", conferenceName)
-	fmt.Println("Get your tickets here to attend")
-
-	var bookings []string
+	greetUsers()
 
 	for {
+		fmt.Printf("\n%d of %d tickets remaining for %s\n\n", nTicketsRemaining, nTickets, conferenceName)
 
-		var firstName string
-		var lastName string
-		var emailAddress string
-		var nTicketsUser uint
+		var booking Booking = getUserInput()
 
+		var canBook = validateUserInput(booking, nTicketsRemaining)
+		if canBook {
+			nTicketsRemaining = updateBookings(booking, nTicketsRemaining)
+			waitGroup.Add(1)
+			go sendEmail(booking, &waitGroup)
+		} else {
+			fmt.Printf("    Please try again.\n\n")
+		}
 		if nTicketsRemaining == 0 {
-			fmt.Printf("\n%v is fully booked\n", conferenceName)
-			var firstNames []string
-			for _, booking := range bookings {
-				firstNames = append(firstNames, strings.Split(booking, " ")[0])
-			}
-			fmt.Printf("These are all the bookings: %v\n\n", firstNames)
+			printFullyBooked()
 			break
 		}
-		fmt.Printf("%d of %d tickets remaining for %s\n", nTicketsRemaining, nTickets, conferenceName)
-		fmt.Println()
-		fmt.Print("    What is your first name?    : ")
-		fmt.Scan(&firstName)
-		fmt.Print("    What is your last name?     : ")
-		fmt.Scan(&lastName)
-		fmt.Print("    How many tickets you want?  : ")
-		fmt.Scan(&nTicketsUser)
-		fmt.Print("    What is your email address? : ")
-		fmt.Scan(&emailAddress)
-		fmt.Printf("\n%s %s (%s) wants to book %d tickets\n", firstName, lastName, emailAddress, nTicketsUser)
-
-		var canBook = nTicketsRemaining >= nTicketsUser
-
-		if canBook {
-			bookings = append(bookings, firstName+" "+lastName)
-			nTicketsRemaining = nTicketsRemaining - nTicketsUser
-		} else {
-			fmt.Println("There's not enough tickets left, try again")
-		}
 	}
+
+	waitGroup.Wait()
+
+}
+
+func greetUsers() {
+	fmt.Printf("\n")
+	fmt.Printf("Welcome to %s booking application\n", conferenceName)
+	fmt.Printf("Get your tickets here to attend\n")
+	fmt.Printf("\n")
+}
+
+func getFirstNames() []string {
+	var firstNames []string
+	for _, booking := range bookings {
+		firstNames = append(firstNames, booking.firstName)
+	}
+	return firstNames
+}
+
+func getUserInput() Booking {
+	var firstName string
+	var lastName string
+	var emailAddress string
+	var nTicketsUser uint
+
+	fmt.Print("    What is your first name?    : ")
+	fmt.Scan(&firstName)
+	fmt.Print("    What is your last name?     : ")
+	fmt.Scan(&lastName)
+	fmt.Print("    How many tickets you want?  : ")
+	fmt.Scan(&nTicketsUser)
+	fmt.Print("    What is your email address? : ")
+	fmt.Scan(&emailAddress)
+	fmt.Printf("\n    %s %s (%s) wants to book %d tickets.\n", firstName, lastName, emailAddress, nTicketsUser)
+
+	var booking = Booking{
+		firstName:    firstName,
+		lastName:     lastName,
+		emailAddress: emailAddress,
+		nTickets:     nTicketsUser,
+	}
+
+	return booking
+}
+
+func updateBookings(booking Booking, nTicketsRemaining uint) uint {
+	bookings = append(bookings, booking)
+	nTicketsRemaining = nTicketsRemaining - booking.nTickets
+	fmt.Printf("    %d tickets booked.\n", booking.nTickets)
+	return nTicketsRemaining
+}
+
+func printFullyBooked() {
+	fmt.Printf("\n%v is now fully booked\n", conferenceName)
+	fmt.Printf("These are all the bookings: %v\n\n", getFirstNames())
+}
+
+func validateUserInput(booking Booking, nTicketsRemaining uint) bool {
+
+	var nameIsValid bool = len(booking.firstName) >= 2 && len(booking.lastName) >= 2
+	var emailAddressIsValid bool = strings.Contains(booking.emailAddress, "@")
+	var nTicketsUserIsValid bool = booking.nTickets > 0
+	var enoughTicketsAvailable bool = nTicketsRemaining >= booking.nTickets
+	var canBook = nameIsValid && emailAddressIsValid && nTicketsUserIsValid && enoughTicketsAvailable
+
+	if !canBook {
+		fmt.Printf("\n    Detected a problem with your booking:\n")
+	}
+	if !nameIsValid {
+		fmt.Println("    - First name or Last name too short")
+	}
+	if !emailAddressIsValid {
+		fmt.Println("    - Invalid email address")
+	}
+	if !nTicketsUserIsValid {
+		fmt.Println("    - Invalid number of tickets")
+	}
+	if !enoughTicketsAvailable {
+		fmt.Println("    - There's not enough tickets left")
+	}
+
+	return canBook
+}
+
+func sendEmail(booking Booking, waitGroup *sync.WaitGroup) {
+	time.Sleep(10 * time.Second)
+	var ticket = fmt.Sprintf("%v tickets for %v %v", booking.nTickets, booking.firstName, booking.lastName)
+	fmt.Printf("\n    Sent %v to email address %v.\n\n", ticket, booking.emailAddress)
+	waitGroup.Done()
 }
